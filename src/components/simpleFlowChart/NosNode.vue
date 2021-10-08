307eab5182a56ccfea10e3de16b205df5ceeb71b
Nurul-GC <nuruldecarvalho@gmail.com>
2021-10-08 22:47:33 +0200

    Update src/components/simpleFlowChart

--- src/components/simpleFlowChart/NosNode.vue
+++ src/components/simpleFlowChart/NosNode.vue
@@ -1,144 +1,140 @@
 <template>
-  <!-- <div  class="flowchart-node terminator" :style="nodeStyle" --> 
- <div  class="Nos-Node" :style="nodeStyle"  
+  <!-- <div  class="flowchart-node terminator" :style="nodeStyle" -->
+  <div
+    class="Nos-Node"
+    :style="nodeStyle"
     @mousedown="handleMousedown"
     @mouseover="handleMouseOver"
     @mouseleave="handleMouseLeave"
-    v-bind:class="{selected: options.selected === id}">
-   <div v-if="type=='terminator'" class="terminator">
-      <div class="node-port node-input"
-       @mousedown="inputMouseDown"
-       @mouseup="inputMouseUp">
-      </div>
+    v-bind:class="{ selected: options.selected === id }"
+  >
+    <div v-if="type == 'terminator'" class="terminator">
+      <div
+        class="node-port node-input"
+        @mousedown="inputMouseDown"
+        @mouseup="inputMouseUp"
+      ></div>
       <div class="node-main">
         <div v-text="type" class="node-type"></div>
         <div v-text="label" class="node-label"></div>
       </div>
-      <div class="node-port node-output" 
-        @mousedown="outputMouseDown">
-      </div>
+      <div class="node-port node-output" @mousedown="outputMouseDown"></div>
       <div v-show="show.delete" class="node-delete">&times;</div>
-   </div>
+    </div>
 
-   <div v-if="type=='manual_input'" class="manual_input">
-      <div class="node-port node-input"
-       @mousedown="inputMouseDown"
-       @mouseup="inputMouseUp">
-      </div>
+    <div v-if="type == 'manual_input'" class="manual_input">
+      <div
+        class="node-port node-input"
+        @mousedown="inputMouseDown"
+        @mouseup="inputMouseUp"
+      ></div>
       <div class="node-main">
         <div v-text="type" class="node-type pt-3 red100"></div>
         <div v-text="label" class="node-label black100"></div>
       </div>
-      <div class="node-port node-output" 
-        @mousedown="outputMouseDown">
-      </div>
+      <div class="node-port node-output" @mousedown="outputMouseDown"></div>
       <div v-show="show.delete" class="node-delete">&times;</div>
-   </div>
-   <div v-if="type=='process'" class="process">
-      <div class="node-port node-input"
-       @mousedown="inputMouseDown"
-       @mouseup="inputMouseUp">
-      </div>
+    </div>
+    <div v-if="type == 'process'" class="process">
+      <div
+        class="node-port node-input"
+        @mousedown="inputMouseDown"
+        @mouseup="inputMouseUp"
+      ></div>
       <div class="node-main">
         <div v-text="type" class="node-type pt-3 yellow100"></div>
         <div v-text="label" class="node-label black100"></div>
       </div>
-      <div class="node-port node-output" 
-        @mousedown="outputMouseDown">
-      </div>
+      <div class="node-port node-output" @mousedown="outputMouseDown"></div>
       <div v-show="show.delete" class="node-delete">&times;</div>
-   </div>
-   <div v-if="type=='display'" class="display ">
-      <div class="node-port node-input"
-       @mousedown="inputMouseDown"
-       @mouseup="inputMouseUp">
-      </div>
-      <div class="node-main ">
+    </div>
+    <div v-if="type == 'display'" class="display">
+      <div
+        class="node-port node-input"
+        @mousedown="inputMouseDown"
+        @mouseup="inputMouseUp"
+      ></div>
+      <div class="node-main">
         <div v-text="type" class="node-type pt-3 green100"></div>
         <div v-text="label" class="node-label black100"></div>
       </div>
-      <div class="node-port node-output" 
-        @mousedown="outputMouseDown">
-      </div>
+      <div class="node-port node-output" @mousedown="outputMouseDown"></div>
       <div v-show="show.delete" class="node-delete">&times;</div>
-   </div>
-   <div v-if="type=='decision'" class="decision ">
-      <div class="node-port node-input"
-       @mousedown="inputMouseDown"
-       @mouseup="inputMouseUp">
-      </div>
-      <div class="node-main ">
+    </div>
+    <div v-if="type == 'decision'" class="decision">
+      <div
+        class="node-port node-input"
+        @mousedown="inputMouseDown"
+        @mouseup="inputMouseUp"
+      ></div>
+      <div class="node-main">
         <div v-text="type" class="node-type pt-3 green100 px-10"></div>
         <div v-text="label" class="node-label black100 px-10 py-20"></div>
       </div>
-      <div class="node-port node-output" 
-        @mousedown="outputMouseDown">
-      </div>
+      <div class="node-port node-output" @mousedown="outputMouseDown"></div>
       <div v-show="show.delete" class="node-delete">&times;</div>
-   </div>
-   <div v-if="type=='preparation'" class="preparation ">
-      <div class="node-port node-input"
-       @mousedown="inputMouseDown"
-       @mouseup="inputMouseUp">
-      </div>
-      <div class="node-main ">
+    </div>
+    <div v-if="type == 'preparation'" class="preparation">
+      <div
+        class="node-port node-input"
+        @mousedown="inputMouseDown"
+        @mouseup="inputMouseUp"
+      ></div>
+      <div class="node-main">
         <div v-text="type" class="node-type pt-3 green100 px-10"></div>
         <div v-text="label" class="node-label black100 px-10 py-20"></div>
       </div>
-      <div class="node-port node-output" 
-        @mousedown="outputMouseDown">
-      </div>
+      <div class="node-port node-output" @mousedown="outputMouseDown"></div>
       <div v-show="show.delete" class="node-delete">&times;</div>
-   </div>
-   <div v-if="type=='predefined_process'" class="predefined_process ">
-      <div class="node-port node-input"
-       @mousedown="inputMouseDown"
-       @mouseup="inputMouseUp">
+    </div>
+    <div v-if="type == 'predefined_process'" class="predefined_process">
+      <div
+        class="node-port node-input"
+        @mousedown="inputMouseDown"
+        @mouseup="inputMouseUp"
+      ></div>
+      <div class="node-main">
+        <div v-text="type" class="node-type pt-3 green100"></div>
+        <div v-text="label" class="node-label black100 py-20"></div>
       </div>
-      <div class="node-main ">
-        <div v-text="type" class="node-type pt-3 green100 "></div>
-        <div v-text="label" class="node-label black100  py-20"></div>
-      </div>
-      <div class="node-port node-output" 
-        @mousedown="outputMouseDown">
-      </div>
+      <div class="node-port node-output" @mousedown="outputMouseDown"></div>
       <div v-show="show.delete" class="node-delete">&times;</div>
-   </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'NosNode',
+  name: "NosNode",
   props: {
     id: {
       type: Number,
       default: 1000,
       validator(val) {
-        return typeof val === 'number'
-      }
+        return typeof val === "number";
+      },
     },
     x: {
       type: Number,
       default: 0,
       validator(val) {
-        return typeof val === 'number'
-      }
-    },    
+        return typeof val === "number";
+      },
+    },
     y: {
       type: Number,
       default: 0,
       validator(val) {
-        return typeof val === 'number'
-      }
+        return typeof val === "number";
+      },
     },
     type: {
       type: String,
-      default: 'Default'
+      default: "Default",
     },
     label: {
       type: String,
-      default: 'input name'
+      default: "input name",
     },
     options: {
       type: Object,
@@ -147,34 +143,36 @@ export default {
           centerX: 1024,
           scale: 1,
           centerY: 140,
-        }
-      }
-    }
+        };
+      },
+    },
   },
   data() {
     return {
       show: {
         delete: false,
-      }
-    }
+      },
+    };
   },
-  mounted() {
-  },
+  mounted() {},
   computed: {
     nodeStyle() {
       return {
-        top: this.options.centerY + this.y * this.options.scale + 'px', // remove: this.options.offsetTop + 
-        left: this.options.centerX + this.x * this.options.scale + 'px', // remove: this.options.offsetLeft + 
+        top: this.options.centerY + this.y * this.options.scale + "px", // remove: this.options.offsetTop +
+        left: this.options.centerX + this.x * this.options.scale + "px", // remove: this.options.offsetLeft +
         transform: `scale(${this.options.scale})`,
-      }
-    }
+      };
+    },
   },
   methods: {
     handleMousedown(e) {
       const target = e.target || e.srcElement;
       // console.log(target);
-      if (target.className.indexOf('node-input') < 0 && target.className.indexOf('node-output') < 0) {
-        this.$emit('nodeSelected', e);
+      if (
+        target.className.indexOf("node-input") < 0 &&
+        target.className.indexOf("node-output") < 0
+      ) {
+        this.$emit("nodeSelected", e);
       }
       e.preventDefault();
     },
@@ -185,18 +183,18 @@ export default {
       this.show.delete = false;
     },
     outputMouseDown(e) {
-      this.$emit('linkingStart')
+      this.$emit("linkingStart");
       e.preventDefault();
     },
     inputMouseDown(e) {
       e.preventDefault();
     },
     inputMouseUp(e) {
-      this.$emit('linkingStop')
+      this.$emit("linkingStop");
       e.preventDefault();
     },
-  }
-}
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -207,13 +205,13 @@ $portSize: 12;
 .Nos-Node {
   margin: 0;
   min-width: 80px;
- /*  height: 80px; */
+  /*  height: 80px; */
   position: absolute;
   box-sizing: border-box;
   border: none;
   background: transparent;
   z-index: 1;
-  opacity: .9;
+  opacity: 0.9;
   cursor: move;
   transform-origin: top left;
   .node-main {
@@ -243,10 +241,10 @@ $portSize: 12;
     }
   }
   .node-input {
-    top: #{-2+$portSize/-2}px;
+    top: #{-2 + $portSize/-2}px;
   }
   .node-output {
-    bottom: #{-2+$portSize/-2}px;
+    bottom: #{-2 + $portSize/-2}px;
   }
   .node-delete {
     position: absolute;
@@ -261,7 +259,7 @@ $portSize: 12;
     border: 1px solid $themeColor;
     border-radius: 100px;
     text-align: center;
-    &:hover{
+    &:hover {
       background: $themeColor;
       color: white;
     }
@@ -271,30 +269,29 @@ $portSize: 12;
   box-shadow: 0 0 0 2px $themeColor;
 }
 
-.flowchart-node{
+.flowchart-node {
   min-width: 100px;
   min-height: 50px !important;
   background: transparent;
-  
 }
-.terminator{
-  .node-main{
+.terminator {
+  .node-main {
     border-radius: 100px;
     overflow: hidden;
   }
-  .node-label{
+  .node-label {
     background: #ffffff;
     border-radius: 0px 0px 100px 100px;
   }
 }
 
-.manual_input{
-  .node-main{
-   clip-path: polygon(0 20%, 100% 9%, 100% 100%, 0% 100%);
+.manual_input {
+  .node-main {
+    clip-path: polygon(0 20%, 100% 9%, 100% 100%, 0% 100%);
   }
 }
-.display{
-  .node-main{
+.display {
+  .node-main {
     clip-path: polygon(100% 0%, 100% 48%, 100% 100%, 25% 100%, 0% 50%, 25% 0%);
   }
   .node-label {
@@ -302,31 +299,44 @@ $portSize: 12;
     padding-right: 10px;
   }
 }
-.decision{
-  .node-main{
+.decision {
+  .node-main {
     clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
   }
   .node-label {
     padding-bottom: 30px;
-   // padding-right: 10px;
-  } 
+    // padding-right: 10px;
+  }
 }
-.preparation{
-  .node-main{
+.preparation {
+  .node-main {
     clip-path: polygon(72% 0, 100% 49%, 76% 100%, 24% 100%, 0% 50%, 25% 0%);
   }
   .node-label {
-   /*  padding-bottom: 30px; */
-   // padding-right: 10px;
-  } 
+    /*  padding-bottom: 30px; */
+    // padding-right: 10px;
+  }
 }
-.predefined_process{
-  .node-main{
-   clip-path: polygon(0% 15%, 6% 15%, 6% 0, 94% 0, 94% 15%, 100% 15%, 100% 85%, 94% 85%, 94% 100%, 6% 100%, 6% 85%, 0% 85%);
+.predefined_process {
+  .node-main {
+    clip-path: polygon(
+      0% 15%,
+      6% 15%,
+      6% 0,
+      94% 0,
+      94% 15%,
+      100% 15%,
+      100% 85%,
+      94% 85%,
+      94% 100%,
+      6% 100%,
+      6% 85%,
+      0% 85%
+    );
   }
   .node-label {
-   /*  padding-bottom: 30px; */
-   // padding-right: 10px;
-  } 
+    /*  padding-bottom: 30px; */
+    // padding-right: 10px;
+  }
 }
 </style>
