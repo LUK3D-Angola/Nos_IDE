851f2f44b7e025ca9044ce1b0c0d0c3b9a65bbde
Nurul-GC <nuruldecarvalho@gmail.com>
2021-10-08 23:00:24 +0200

    Update src/plugins

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,9 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
-Vue.use(Vuetify,{
-    iconfont:'md',
-    theme:{
-        
-    }
+Vue.use(Vuetify, {
+    iconfont: 'md',
+    theme: {}
 });
 
 export default new Vuetify({});
